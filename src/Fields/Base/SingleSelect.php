bc380b38274227c02bd60c66abf19af54ccae463
Anže Časar <anze.casar@unitednetworks.be>
2015-06-15 12:21:50 +0200

    Added select element

--- src/Fields/Base/SingleSelect.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Laraplus\Form\Fields\Base;
-
-abstract class MultiSelect extends Select
-{
-
-}
--- src/Fields/Select.php
+++ src/Fields/Select.php
@@ -1,6 +1,110 @@
 <?php namespace Laraplus\Form\Fields;
 
-class Select
-{
+use Laraplus\Form\Fields\Base\Element;
 
+class Select extends Element
+{
+    protected $options = [];
+
+    protected $optionAttributes = [];
+
+    /**
+     * @param array $options
+     * @return $this
+     */
+    public function options($options)
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function addOption($key, $value)
+    {
+        $this->options[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @param $name
+     * @param $value
+     * @return $this
+     */
+    public function optionAttribute($key, $name, $value)
+    {
+        if (!isset($this->optionAttributes[$key])) {
+            $this->optionAttributes[$key] = [];
+        }
+
+        $this->optionAttributes[$key][$name] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @param $name
+     * @param $value
+     * @return $this
+     */
+    public function optionData($key, $name, $value)
+    {
+        $this->setAttribute($key, 'data-' . $name, $value);
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function addOptionClass($key, $value)
+    {
+        $class = isset($this->optionAttributes[$key]['class']) ? $this->optionAttributes[$key]['class'] : '';
+
+        $this->setAttribute($key, 'class', trim($class . ' ' . $value));
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function setOptionClass($key, $value)
+    {
+        $this->setAttribute($key, 'class', $value);
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        $select = '<select'.$this->renderAttributes($this->attributes).'>';
+
+        $selected = $this->getValue();
+
+        foreach($this->options as $key => $value) {
+
+            $attr = $key == $selected ? ' selected' : '';
+
+            $select .= '<option value="' . $key . '"' . $attr . '>' . $value . '</option>';
+        }
+
+        $select .= '</select>';
+
+        return $select;
+    }
 }
